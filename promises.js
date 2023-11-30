d8c8c9d519effd62e2318c265e6ba41bf074abf7
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-30 22:01:28 +0100

    update: handling promises

--- promises.js
+++ promises.js
@@ -18,3 +18,35 @@ getSomething()
     .catch(errr => {
         console.log(errr);
     })
+
+    const getTodos = (resource) => {
+
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+        
+            // Event listener to track the state changes of the request
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        const data = JSON.parse(request.responseText);
+                        resolve(data)
+                    } else {
+                        reject("error getting resource");
+                    }
+                }
+            });
+        
+            // Opening a GET request to 'todos.json'
+            request.open('GET', resource);
+            request.send();
+        })
+    }
+    
+    // Invoking the asynchronous function with a callback
+    getTodos('./todos/a.json')
+            .then(data => {
+                console.log("promise resolved: ", data);
+            })
+            .catch(err => {
+                console.log("promise rejected: ", err);
+            })
